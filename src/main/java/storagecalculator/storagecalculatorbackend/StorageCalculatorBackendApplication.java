3cc9e9934842d1f6e383ccfbf280c1ccfe1689ca
Student03 <aaronsantiago542@gmail.com>
2024-03-01 14:17:55 -0500

    first

---
+++ src/main/java/storagecalculator/storagecalculatorbackend/StorageCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package storagecalculator.storagecalculatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/storagecalculator/storagecalculatorbackend/StorageCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package storagecalculator.storagecalculatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
